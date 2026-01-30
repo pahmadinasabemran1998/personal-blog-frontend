5cdf47decd02e7cd147841b2eb4389a39d70f2f6
Parsa Ahmadi Nasab Emran <parsatempleowl@brandeis.edu>
2026-01-30 09:33:07 -0500

    feat: create PostList component to display posts with edit/delete

---
+++ src/components/PostList.jsx
@@ -0,0 +1,27 @@
+const PostList = ({ posts, onEdit, onDelete }) => {
+    if (!posts.length) {
+        return <p>No posts found. Start writing your first post!</p>;
+    }
+
+    return (
+        <div>
+            {posts.map((post) => {
+                <div
+                    key={post._id}
+                    style={{
+                        border: "1px solid #ccc",
+                        padding: "1rem",
+                        marginBottom: "1rem"
+                    }}
+                >
+                    <h3>{post.title}</h3>
+                    <p>{post.content}</p>
+                    <button onClick={() => onEdit(post)}>Edit</button>
+                    <button onClick={() => onDelete(post._id)}>Delete</button>
+                </div>
+            })}
+        </div>
+    );
+};
+
+export default PostList;
